ecc5013ee95ab9ecdff4f0fb743791fdb9def273
Nathan Lewis <nathan@differential.com>
2024-05-31 08:33:34 -0600

    chore: fix 263c

--- src/game/gameState.js
+++ src/game/gameState.js
@@ -80,7 +80,7 @@ export async function initializeGame() {
     setGameData('locationTables', locationTables)
     setGameData('weapons', weapons)
     startGame()
-    displayEntry('263') // Ensure the first entry is displayed ... should be 13
+    displayEntry('263c') // Ensure the first entry is displayed ... should be 13
   } catch (error) {
     console.error('There has been a problem with your fetch operation:', error)
   }
