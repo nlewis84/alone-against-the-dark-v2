c5daa5b9302cf770cdc6e931ced412eaa5018e4c
Nathan Lewis <nathan@differential.com>
2024-06-20 19:14:25 -0500

    chore: add lots of new entries and also clean up checks per day

--- src/game/gameActions.js
+++ src/game/gameActions.js
@@ -173,7 +173,10 @@ function handleEntryChoices(entryId, entry) {
       ? canUseSkill(entryId, choice.effects.check.skill)
       : true // Only call canUseSkill if there's a skill to check
 
-    if (checkRequirements(choice.requirements) && canUseSkillToday) {
+    if (
+      checkRequirements(choice.requirements) &&
+      (canUseSkillToday || choice.effects.dailyLimit === null)
+    ) {
       const button = createButton(
         choice.text,
         'px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 mb-2',
